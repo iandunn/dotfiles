2d9f6d092bb6d1c2f4e18c997925afa1e4518d85
Ian Dunn <ian@iandunn.name>
2014-08-16 14:07:37 -0700

    Save screenshots to the Downloads directory instead of the Desktop

--- osx.sh
+++ osx.sh
@@ -28,5 +28,11 @@ defaults write com.apple.finder EmptyTrashSecurely -bool true
 ### Misc
 ###
 
-# Make application bar appear slowly b/c i want it hidden most of the time
+# Increase the dock appear delay to ~4 seconds. I never use it and it pops up when I am trying to hover on horizontal scroll bars in Chrome.
+# TODO
 
+# Remove or reduce window shadows to avoid them overlapping full screen windows.
+# TODO - There doesn't seem to be a good, reliable solution to this, but keep looking.
+
+# Save screenshots to the Downloads directory instead of the Desktop
+defaults write com.apple.screencapture location ~/Downloads/
