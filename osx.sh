2f2eef67208bd611296f4e5cf04cea9e476150c6
Ian Dunn <ian@iandunn.name>
2014-08-16 13:36:00 -0700

    Show hidden files in the Open dialog

--- osx.sh
+++ osx.sh
@@ -4,11 +4,17 @@
 ### Finder
 ###
 
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
 # Show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Show hidden files in the Open dialog
+defaults write -g AppleShowAllFiles -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
 
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
@@ -16,11 +22,6 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 # Empty Trash securely by default
 defaults write com.apple.finder EmptyTrashSecurely -bool true
 
-# Show the ~/Library folder
-chflags nohidden ~/Library
-
-# Show hidden files in the Open dialog. Not sure if there's a default for this.
-# TODO
 
 
 ###
