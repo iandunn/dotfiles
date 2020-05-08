243701ba9597f3d6e8fef18b4a65fdd8a94c193e
Ian Dunn <ian@iandunn.name>
2020-05-08 10:50:29 -0700

    Homebrew: Add skeleton of install script

---
+++ bin/install-homebrew-packages.sh
@@ -0,0 +1,9 @@
+# todo write script to re-install these on new systems
+
+# httpd and/or httpd24
+# hub z
+# php-code-sniffer  phpmd phpunit mailhog mkcert
+# sass composer mysql telnet
+# nginx wget nvm gunpg openssl
+
+
