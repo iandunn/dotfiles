88b988ccf33a7d46b7ed2ea06d3d72d00cb1b359
Ian Dunn <ian@iandunn.name>
2022-03-12 10:10:35 -0800

    homebrew: add packages

--- bin/install-homebrew-packages.sh
+++ bin/install-homebrew-packages.sh
@@ -1,14 +1,30 @@
 # todo write script to re-install these on new systems
 
-# hub z
-# gls gdate
-# mailhog mkcert
-# sass composer mysql telnet
-# nginx wget nvm gunpg openssl
+# hub
+# z
+# mailhog
+# mkcert nss
+# composer
+# mysql
+# telnet
+# nginx
+# php
+# coreutils
+# lnav
+# wget nvm
+# gunpg openssl
 # svn
+
+#pecl install xdebug
+
+brew services start mailhog
+brew services start mysql
+brew services start php
+mkcert -install
 
 # also want https://github.com/bobthecow/psysh/wiki/Installation, which doesn't have an brew package
 
 # don't need anymore?
 # httpd and/or httpd24
-# php-code-sniffer phpmd phpunit  - installed via composer into local packages, not used globally?
+# php-code-sniffer phpmd phpunit sass - installed via composer into local packages, not used globally?
+#
