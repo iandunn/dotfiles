30601179b643bb91d8b2e12a3c0b965462f2a2e9
Ian Dunn <ian@iandunn.name>
2023-01-03 16:51:32 -0800

    ordinaryish: Tweak `MOUSEKEY_INTERVAL` and `MOUSEKEY_TIME_TO_MAX`

--- keyboards/ergodox-ez/ordinaryish/config.h
+++ keyboards/ergodox-ez/ordinaryish/config.h
@@ -1,19 +1,37 @@
 // See https://docs.qmk.fm/#/config_options?id=the-configh-file
 
+/*
+ * Misc
+ */
+#define FIRMWARE_VERSION u8"wEZWj/QM7mp"
+
 #undef ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 2885
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
+
+
+/*
+ * Mouse Keys
+ */
 #undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY 0
-
 #undef MOUSEKEY_WHEEL_INTERVAL
-#define MOUSEKEY_WHEEL_INTERVAL 75
-
 #undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_TIME_TO_MAX
+
+#define MOUSEKEY_INTERVAL 25
+#define MOUSEKEY_TIME_TO_MAX 80
+
+#define MOUSEKEY_WHEEL_DELAY 0
+#define MOUSEKEY_WHEEL_INTERVAL 65
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 80
 
-#define FIRMWARE_VERSION u8"wEZWj/QM7mp"
+
+
+/*
+ * Home Row Mods
+ */
 
 // Necessary for home row mods to work well
 #define IGNORE_MOD_TAP_INTERRUPT
