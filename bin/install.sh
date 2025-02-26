5157cc9df739cb3e116625cb01bf45cb23b8fce8
Ian Dunn <ian@iandunn.name>
2025-02-26 14:44:35 -0800

    Bin: Add and install iterm2 preferences, refine install

--- bin/install.sh
+++ bin/install.sh
@@ -6,6 +6,13 @@
 # Some things, like SSH, phpcs, and phpmd aren't installed, because they're only used on a few machines.
 #
 
+printf "WARNING: This will overwrite files like ~/.bashrc and ~/.bash_profile. \nHave you diff'd them against the ~/dotfiles version and integrated any changes? (y/N) "
+read choice
+
+if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
+  echo "Aborting."
+  exit 1
+fi
 
 if [ -d $HOME/dotfiles ]; then
 	printf "\nInstalling from $HOME/dotfiles\n"
@@ -24,41 +31,23 @@ else
 fi
 
 # Overwrite existing files, because the defaults are unlikely to be useful or important.
-ln -sf $DOTFILES_DIR/.bash_aliases	$HOME/.bash_aliases
+ln -sf $DOTFILES_DIR/.bashrc		$HOME/.bashrc
+ln -sf $DOTFILES_DIR/.bash_profile	$HOME/.bash_profile
 ln -sf $DOTFILES_DIR/.bash_prompt	$HOME/.bash_prompt
+ln -sf $DOTFILES_DIR/.bash_aliases	$HOME/.bash_aliases
 ln -sf $DOTFILES_DIR/.gitconfig		$HOME/.gitconfig
 ln -sf $DOTFILES_DIR/.gitignore_global	$HOME/.gitignore_global
 ln -sf $DOTFILES_DIR/.inputrc		$HOME/.inputrc
 ln -sf $DOTFILES_DIR/.config/micro	$HOME/.config/micro/
 
+ln -sf ~/dotfiles/iterm2/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+
 # These have to be hard links, because Subversion and SSH configs don't support symlinks.
-ln -f $DOTFILES_DIR/.ssh/config		$HOME/.ssh/config
+mkdir $HOME/.ssh
+mkdir $HOME/.subversion
+ln -f $DOTFILES_DIR/.ssh/config			$HOME/.ssh/config
 ln -f $DOTFILES_DIR/.subversion/config	$HOME/.subversion/config
 ln -f $DOTFILES_DIR/.subversion/servers	$HOME/.subversion/servers
-
-
-
-# Append to existing files, to avoid overwriting possibly useful/important system defaults.
-if [ -f $HOME/.bashrc ]; then
-	COMMAND="source $DOTFILES_DIR/.bashrc"
-
-	if [ ! -L $HOME/.bashrc ] && ! grep -q "$COMMAND" "$HOME/.bashrc"; then
-		printf "\nsource $DOTFILES_DIR/.bashrc\n" >> $HOME/.bashrc
-	fi
-else
-	ln -s $DOTFILES_DIR/.bashrc $HOME/.bashrc
-fi
-
-# todo make this DRY w/ block above
-if [ -f $HOME/.bash_profile ]; then
-	COMMAND="source $DOTFILES_DIR/.bash_profile"
-
-	if [ ! -L $HOME/.bash_profile ] && ! grep -q "$COMMAND" "$HOME/.bash_profile"; then
-		printf "\n$COMMAND\n" >> $HOME/.bash_profile
-	fi
-else
-        ln -s $DOTFILES_DIR/.bash_profile $HOME/.bash_profile
-fi
 
 cat << COW
 
