a5dbda5faf27ee1de67b00a044d0414c6d7dd66d
Ian Dunn <ian@iandunn.name>
2025-07-18 08:43:09 -0700

    brew: install ngrok

--- bin/install-homebrew-packages.sh
+++ bin/install-homebrew-packages.sh
@@ -11,6 +11,13 @@ packages=(
   jq
   lnav
   micro
+
+  # Avast will flag, see https://github.com/ngrok/homebrew-ngrok/issues/21.
+  # To install it, open Avast and go to Core Shields and turn off File Shield, run the install, then turn File Shield back on.
+  # Then open ngrok. Avast will block it, but allow you to add it to the exceptions.
+  # To use it, run `ngrok http --host-header=foo.test 80`
+  ngrok
+
   nvm
   #php@8.2 # probably remove this b/c localwp installs its own php. when this is linked, it overrides the localwp php and throws errors about extensions not being found etc. have to run `brew unlink php` every time homebrew updates.
   phpunit
