f350665e1795aea9958b4bdd32021ebddfe1d8db
Ian Dunn <ian@iandunn.name>
2025-09-18 13:12:28 -0700

    aliases: remove `omp` because `oh<tab>` is easier

--- bash/aliases/miscellaneous.sh
+++ bash/aliases/miscellaneous.sh
@@ -7,7 +7,6 @@ alias zinc='zoxide add .'
 alias watch='fswatch -o $(pwd) |xargs -n1 -I{} $1'
 
 alias mic='micro'
-alias omp='oh-my-posh'
 alias soal='source ~/.bash_aliases'
 alias soba='source ~/.bashrc'
 
