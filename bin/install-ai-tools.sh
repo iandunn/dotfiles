c55f77c393dfb3502bc467ab3534969a7cc49c51
Ian Dunn <ian@iandunn.name>
2026-03-06 07:54:23 -0800

    bin: add script to update claude skills etc

---
+++ bin/install-ai-tools.sh
@@ -0,0 +1,24 @@
+#
+# WP Agent Skills
+#
+SKILLS_DIR="$HOME/vhosts/tools/wp-agent-skills"
+
+if [ -d "$SKILLS_DIR" ]; then
+    git -C "$SKILLS_DIR" pull
+else
+    git clone https://github.com/WordPress/agent-skills.git "$SKILLS_DIR"
+fi
+
+cd "$SKILLS_DIR"
+node shared/scripts/skillpack-build.mjs --clean
+
+# Need to periodically update this list as thing evolve. Don't install ones that 10up has a fork of though.
+node shared/scripts/skillpack-install.mjs --global --skills=wp-playground,wp-abilities-api,wp-wpcli-and-ops,wp-phpstan
+
+
+#
+# 10up Agent Skills
+#
+npx @10up/agent-skills --global
+
+# what else? custom subagents?
